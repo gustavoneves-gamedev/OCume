6a101ce4d11e29609622e42d308df0bd094affd8
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-01-26 21:12:45 -0300

    fix: Pause Menu active
    Corrigi o erro em que o Menu de Pause não desaparecia

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -41,6 +41,9 @@ public class UIController : MonoBehaviour
 
     public void BackToMainMenu()
     {
+        pauseMenu.SetActive(false);
+
+
         playerRoot.EndRun();
 
         mainMenu.SetActive(true);
