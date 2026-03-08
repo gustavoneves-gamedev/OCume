9648306f0ddd96daf1a607f2133ae9f43fbdac61
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-03-08 16:21:14 -0300

    fix: Obstacle Root collision
    Corrigi o erro de não conseguir destruir os obstáculos depois de um tempo

--- Assets/Scripts/ObstacleRoot.cs
+++ Assets/Scripts/ObstacleRoot.cs
@@ -7,7 +7,7 @@ public class ObstacleRoot : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject, 30f);
     }
 
     // Update is called once per frame
@@ -20,8 +20,14 @@ public class ObstacleRoot : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            obstacle.SetActive(false);
+            Debug.Log("Colidi com algo");
             Destroy(gameObject, 1f);
+            obstacle.SetActive(false);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
