47b5cc1fdfc31e995ea65c5d3593988b6cabcfdc
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-03-26 01:38:36 -0300

    feat: Created coin script
    Criei script para moeda e o configurei para detectar colisão com o Player e alterando o status corretamente

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private int coinID = 0;
+    private MeshRenderer meshRenderer;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Destroy(gameObject,30f);
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -18,8 +22,11 @@ public class Coin : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
-            Destroy(gameObject);
+            if (coinID == 0) GameController.gameController.UpdateRunCoins(1, 0);
+            if (coinID == 1) GameController.gameController.UpdateRunCoins(0, 1);
+
+            meshRenderer.enabled = false;
+            Destroy(gameObject, 2f);
         }
     }
 
