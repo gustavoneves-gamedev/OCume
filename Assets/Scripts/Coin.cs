be67ba7c2182fbf561bd170c1d64f57512cf818b
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-03-26 20:22:07 -0300

    fix: Coin stack reset
    Corrigi o código para deletar corretamente os stacks de moedas ao final da Run

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,18 +4,20 @@ public class Coin : MonoBehaviour
 {
     [SerializeField] private int coinID = 0;
     private MeshRenderer meshRenderer;
+    private GameObject parent;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Destroy(gameObject,30f);
-        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer = GetComponent<MeshRenderer>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameController.gameController.isRunning) return;
+        Destroy(parent, 1f);
     }
 
     private void OnTriggerEnter(Collider other)
