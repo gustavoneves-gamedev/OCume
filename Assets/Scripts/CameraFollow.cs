fc259ddc65c2663446fb888e16b30b3afacb6cca
Gustavo Neves <gustavo.neves.1995@gmail.com>
2026-04-28 08:25:41 -0300

    feat: Created Camera Script
    Criei um script para a câmera seguir o jogador de forma mais suave

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,7 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Transform targetToFollow;
+    [SerializeField] private float cameraSpeed = 10f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,8 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Mathf.Lerp(transform.position.x,targetToFollow.position.x + positionOffset.x, cameraSpeed * Time.deltaTime);
+        //float x = Mathf.Lerp(transform.position.x, targetToFollow.position.x + positionOffset.x, 0.5f);
+        transform.position = new Vector3(x, transform.position.y, targetToFollow.position.z + positionOffset.z);
     }
 }
